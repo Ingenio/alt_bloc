8118e05844ae27ab86508500b22e5366a40c5eb4
Yegor Logachev <egor@itomy.ch>
2020-01-22 22:31:34 +0200

    BlocBuilder has been refactored

--- lib/src/bloc_builder.dart
+++ lib/src/bloc_builder.dart
@@ -5,30 +5,29 @@ import 'package:flutter/widgets.dart';
 import 'bloc.dart';
 import 'bloc_provider.dart';
 
-typedef BlocWidgetBuilder<US> = Widget Function(BuildContext context, US state);
+typedef BlocWidgetBuilder<S> = Widget Function(BuildContext context, S state);
 
 /// Bloc Builder that observe Bloc by subscribing on StreamController.
-class BlocBuilder<B extends Bloc, US> extends StatefulWidget {
-  final BlocWidgetBuilder<US> builder;
+class BlocBuilder<B extends Bloc, S> extends StatefulWidget {
+  final BlocWidgetBuilder<S> builder;
   final B bloc;
 
-  const BlocBuilder({this.bloc, @required this.builder});
+  const BlocBuilder({Key key, this.bloc, @required this.builder}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() => _BlocBuilderState<B, US>();
+  State<StatefulWidget> createState() => _BlocBuilderState<B, S>();
 }
 
-class _BlocBuilderState<B extends Bloc, US> extends State<BlocBuilder<B, US>> {
-
-  StreamSubscription<US> _subscription;
-  US _data;
+class _BlocBuilderState<B extends Bloc, S> extends State<BlocBuilder<B, S>> {
+  StreamSubscription<S> _subscription;
+  S _data;
   B _bloc;
 
   @override
   void initState() {
     super.initState();
     _bloc = widget.bloc ?? Provider.of<B>(context);
-    _data = _bloc?.initialState<US>();
+    _data = _bloc?.initialState<S>();
     _subscribe();
   }
 
@@ -36,12 +35,12 @@ class _BlocBuilderState<B extends Bloc, US> extends State<BlocBuilder<B, US>> {
   Widget build(BuildContext context) => widget.builder(context, _data);
 
   @override
-  void didUpdateWidget(BlocBuilder<B, US> oldWidget) {
+  void didUpdateWidget(BlocBuilder<B, S> oldWidget) {
     super.didUpdateWidget(oldWidget);
-    final oldBloc = oldWidget.bloc ?? Provider.of<B>(context);
-    final currentBloc = widget.bloc ?? oldBloc;
+    final oldBloc = oldWidget.bloc;
+    final currentBloc = widget.bloc ?? Provider.of<B>(context);
     if (oldBloc != currentBloc) {
-      _bloc = widget.bloc ?? Provider.of<B>(context);
+      _bloc = currentBloc;
       if (_subscription != null) {
         _unsubscribe();
       }
@@ -50,7 +49,7 @@ class _BlocBuilderState<B extends Bloc, US> extends State<BlocBuilder<B, US>> {
   }
 
   void _subscribe() {
-    _subscription = _bloc?.listenState<US>((US data) {
+    _subscription = _bloc?.listenState<S>((S data) {
       setState(() {
         _data = data;
       });
